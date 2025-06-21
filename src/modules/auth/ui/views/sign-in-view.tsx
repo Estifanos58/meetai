d42197ab6faa38649d8c76a56ee34dc5d26ec098
Estifanos58 <estifkebe08@gmail.com>
2025-06-21 22:26:10 +0300

    update: signup page in progress

--- src/modules/auth/ui/views/sign-in-view.tsx
+++ src/modules/auth/ui/views/sign-in-view.tsx
@@ -17,12 +17,19 @@ import { Alert, AlertTitle } from "@/components/ui/alert";
 import { OctagonAlertIcon } from "lucide-react";
 import { useForm } from "react-hook-form";
 import Link from "next/link";
+import { useRouter } from "next/router";
+import { useState } from "react";
+import { authClient } from "@/lib/auth-client";
 
 const formSchema = z.object({
   email: z.string().email("Invalid email address"),
   password: z.string().min(1, "Password is required"),
 });
 export const SignInView = () => {
+  const router = useRouter();
+  const [error, setError] = useState<string | null>(null);
+  const [pending, setPending] = useState(false);
+
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -31,85 +38,110 @@ export const SignInView = () => {
     },
   });
 
+  const onSubmit = (data: z.infer<typeof formSchema>) => {
+    setError(null);
+    setPending(true);
+    authClient.signIn.email(
+      {
+        email: data.email,
+        password: data.password,
+      },
+      {
+        onSuccess: () => {
+          setPending(false);
+          router.push("/");
+        },
+        onError: (error) => {
+          setPending(false);
+          setError(error.error.message);
+        },
+      }
+    );
+  };
+
   return (
     <div className="flex flex-col gap-6">
       <Card className="overflow-hidden p-0">
         <CardContent className="grid p-0 md:grid-cols-2">
           <Form {...form}>
-            <form className="p-6 md:p-8">
+            <form onSubmit={form.handleSubmit(onSubmit)} className="p-6 md:p-8">
               <div className="flex flex-col gap-6">
                 <div className="flex flex-col items-center text-center">
                   <h1 className="text-2xl font-bold">Welcome back</h1>
-                  <p className="text-muted-foreground text-balance"> Login to your account</p>
+                  <p className="text-muted-foreground text-balance">
+                    {" "}
+                    Login to your account
+                  </p>
                 </div>
                 <div className="grid gap-3">
-                    <FormField
-                      control={form.control}
-                      name="email"
-                      render={({ field }) => (
-                        <FormItem>
-                            <FormLabel>Email</FormLabel>
-                            <FormControl>
-                              <Input
-                                type="email"
-                                placeholder="Enter your email"
-                                {...field}
-                              />
-                            </FormControl>
-                            <FormMessage />
-                        </FormItem>
-                      )}
-                    />
+                  <FormField
+                    control={form.control}
+                    name="email"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Email</FormLabel>
+                        <FormControl>
+                          <Input
+                            type="email"
+                            placeholder="Enter your email"
+                            {...field}
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
                 </div>
-                 <div className="grid gap-3">
-                    <FormField
-                      control={form.control}
-                      name="password"
-                      render={({ field }) => (
-                        <FormItem>
-                            <FormLabel>Password</FormLabel>
-                            <FormControl>
-                              <Input
-                                type="password"
-                                placeholder="Enter your password"
-                                {...field}
-                              />
-                            </FormControl>
-                            <FormMessage />
-                        </FormItem>
-                      )}
-                    />
+                <div className="grid gap-3">
+                  <FormField
+                    control={form.control}
+                    name="password"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Password</FormLabel>
+                        <FormControl>
+                          <Input
+                            type="password"
+                            placeholder="Enter your password"
+                            {...field}
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
                 </div>
-                {true && (
-                    <Alert className="bg-destructive/10 border-none">
-                        <OctagonAlertIcon className="h-4 w-4 !text-destructive"/>
-                        <AlertTitle>Error</AlertTitle>
-                    </Alert>
+                {!error && (
+                  <Alert className="bg-destructive/10 border-none">
+                    <OctagonAlertIcon className="h-4 w-4 !text-destructive" />
+                    <AlertTitle>{error}</AlertTitle>
+                  </Alert>
                 )}
-                <Button className="w-full" type="submit">
-                    Sign In
+                <Button disabled={pending} className="w-full" type="submit">
+                  Sign In
                 </Button>
                 <div className="after:border-border relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after-flex after-items-center after:border-t">
-                    <span className="bg-card text-muted-foreground relative z-10 px-2"> Or continue with</span>
+                  <span className="bg-card text-muted-foreground relative z-10 px-2">
+                    {" "}
+                    Or continue with
+                  </span>
                 </div>
                 <div className="grid grid-cols-2 gap-4">
-                    <Button 
-                       variant="outline"
-                       type="button"
-                       className="w-full"
-                    >
-                        Google
-                    </Button>
-                    <Button 
-                       variant="outline"
-                       type="button"
-                       className="w-full"
-                    >
-                        Github
-                    </Button>
+                  <Button variant="outline" type="button" className="w-full">
+                    Google
+                  </Button>
+                  <Button variant="outline" type="button" className="w-full">
+                    Github
+                  </Button>
                 </div>
                 <div className="text-center tex-sm">
-                    Don&apos;t have an account? <Link href={"/sign-up"} className="underline underline-offset-4">Sign Up</Link>
+                  Don&apos;t have an account?{" "}
+                  <Link
+                    href={"/sign-up"}
+                    className="underline underline-offset-4"
+                  >
+                    Sign Up
+                  </Link>
                 </div>
               </div>
             </form>
@@ -127,7 +159,8 @@ export const SignInView = () => {
       </Card>
 
       <div className="text-muted-foreground *:[a]:hover:text-primary text-center text-xs text-balance *:[a]:underline *:[a]:underline-offset-4">
-            By clickig continue, you agree to our <a href="">Terms of Service</a> and <a href="">Privacy Policy</a>.
+        By clickig continue, you agree to our <a href="">Terms of Service</a>{" "}
+        and <a href="">Privacy Policy</a>.
       </div>
     </div>
   );
