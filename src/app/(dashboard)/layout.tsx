2226202d070df564f9c0fac69299e42b54eb2e8f
Estifanos58 <estifkebe08@gmail.com>
2025-06-26 18:00:15 +0300

    update: dashboard nav in progress

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,4 +1,5 @@
 import { SidebarProvider } from "@/components/ui/sidebar";
+import { DashboardNavbar } from "@/modules/dashboard/ui/components/dashboard-navbar";
 import DashboardSidebar from "@/modules/dashboard/ui/components/dashboard-sidebar";
 import React from 'react'
 
@@ -6,8 +7,9 @@ function layout({ children }: { children: React.ReactNode }) {
   return (
     <SidebarProvider>
          <DashboardSidebar/>
-         <main className="">
-         {children}
+         <main className="flex flex-col h-screen w-screen bg-muted">
+            <DashboardNavbar/>
+            {children}
          </main>
      </SidebarProvider>
   )
