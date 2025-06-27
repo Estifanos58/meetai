18a654ab33e2fefa7923e9d66f91d9df46a9d473
Estifanos58 <estifkebe08@gmail.com>
2025-06-27 18:02:37 +0300

    update : agent schema added and trpc prefetching in the server side added with loading and error state

---
+++ src/app/(dashboard)/agents/error.tsx
@@ -0,0 +1,12 @@
+'use client'
+
+import { AgentsErrorView } from '@/modules/agents/ui/views/agents-view'
+import React from 'react'
+
+function error() {
+  return (
+    <AgentsErrorView/>
+  )
+}
+
+export default error
