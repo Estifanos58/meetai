cbb256d1c286cd268b16cebd52b3c547d4a0d2bd
Estifanos58 <estifkebe08@gmail.com>
2025-06-27 08:07:00 +0300

    open dashboard dialog

--- src/modules/dashboard/ui/components/dashboard-command.tsx
+++ src/modules/dashboard/ui/components/dashboard-command.tsx
@@ -6,7 +6,7 @@ interface Props {
     setOpen: Dispatch<SetStateAction<boolean>>
 }
 
-export const DashboardCommand = ({open: setOpen}:Props) => {
+export const DashboardCommand = ({ open, setOpen }: Props) => {
     return (
         <CommandDialog open={open} onOpenChange={setOpen}>
             <CommandInput placeholder="Find a meeting or agent"/>
