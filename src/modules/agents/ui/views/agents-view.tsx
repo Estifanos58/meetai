a048b2021d19606ab97468e947d71340ee239312
Estifanos58 <estifkebe08@gmail.com>
2025-06-29 16:03:52 +0300

    feat: pagination added for the table and search added

--- src/modules/agents/ui/views/agents-view.tsx
+++ src/modules/agents/ui/views/agents-view.tsx
@@ -6,16 +6,34 @@ import { ErrorState } from "@/components/error-state";
 import { DataTable } from "../components/data-table";
 import { columns } from "../components/columns";
 import { EmptyState } from "@/components/empty-state";
+import { useAgentsFilters } from "../../hooks/use-agents-filters";
+import { DataPagination } from "../components/data-pagination";
 
 export const AgentsView = () => {
+  const [filters, setFilters] = useAgentsFilters();
   const trpc = useTRPC();
-  const { data } = useSuspenseQuery(trpc.agents.getMany.queryOptions({}));
+  const { data } = useSuspenseQuery(
+    trpc.agents.getMany.queryOptions({
+      search: filters.search,
+    })
+  );
 
-  
-  return <div className="flex-1 pb-4 px-4 md:px-8 flex flex-col gap- y-4 ">
-    <DataTable data={data.item } columns={columns} />
-    {data.item.length === 0 && <EmptyState title="Create your first Agent" description="Create an agent to Join your meetings. Each agent will follow your instructions and can interact with participants during the chat."/>}
-  </div>;
+  return (
+    <div className="flex-1 pb-4 px-4 md:px-8 flex flex-col gap- y-4 ">
+      <DataTable data={data.item} columns={columns} />
+      <DataPagination
+        page={filters.page}
+        totalPages={data.totalPages}
+        onPageChange={(page) => setFilters({ page })}
+      />
+      {data.item.length === 0 && (
+        <EmptyState
+          title="Create your first Agent"
+          description="Create an agent to Join your meetings. Each agent will follow your instructions and can interact with participants during the chat."
+        />
+      )}
+    </div>
+  );
 };
 
 export const AgentsLoadingView = () => {
