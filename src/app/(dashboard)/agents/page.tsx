31a98aae168fa6d1b0be7ea61cb01a7a9f6979dd
Estifanos58 <estifkebe08@gmail.com>
2025-06-29 12:43:47 +0300

    update: fileter in progress

--- src/app/(dashboard)/agents/page.tsx
+++ src/app/(dashboard)/agents/page.tsx
@@ -16,7 +16,7 @@ async function page() {
           redirect("/sign-in")
       }
     const queryClient = getQueryClient();
-    void queryClient.prefetchQuery(trpc.agents.getMany.queryOptions());
+    void queryClient.prefetchQuery(trpc.agents.getMany.queryOptions({}));
   return (
     <>
     <ListHeader />
--- src/modules/agents/ui/views/agents-view.tsx
+++ src/modules/agents/ui/views/agents-view.tsx
@@ -9,12 +9,12 @@ import { EmptyState } from "@/components/empty-state";
 
 export const AgentsView = () => {
   const trpc = useTRPC();
-  const { data } = useSuspenseQuery(trpc.agents.getMany.queryOptions());
+  const { data } = useSuspenseQuery(trpc.agents.getMany.queryOptions({}));
 
   
-  return <div className="flex-1 pb-4 px-4 md:px-8 flex flex-col gap-y-4 ">
-    <DataTable data={data} columns={columns} />
-    {data.length === 0 && <EmptyState title="Create your first Agent" description="Create an agent to Join your meetings. Each agent will follow your instructions and can interact with participants during the chat."/>}
+  return <div className="flex-1 pb-4 px-4 md:px-8 flex flex-col gap- y-4 ">
+    <DataTable data={data.item } columns={columns} />
+    {data.item.length === 0 && <EmptyState title="Create your first Agent" description="Create an agent to Join your meetings. Each agent will follow your instructions and can interact with participants during the chat."/>}
   </div>;
 };
 
