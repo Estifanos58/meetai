20c55d3650f027a5cafca6b9913f16c47a1b311d
Estifanos58 <estifkebe08@gmail.com>
2025-06-30 12:26:05 +0300

    update: confirmation modal in progress

--- src/modules/agents/server/procedures.ts
+++ src/modules/agents/server/procedures.ts
@@ -5,7 +5,7 @@ import {
 } from "@/trpc/init";
 import { db } from "@/db";
 import { agents } from "@/db/schema";
-import { agentsInsertSchema } from "../schemas";
+import { agentsInsertSchema, agentsUpdateSchema } from "../schemas";
 import { z } from "zod";
 import { and, count, desc, eq, getTableColumns, ilike, sql } from "drizzle-orm";
 import { DEFAULT_PAGE, DEFAULT_PAGE_SIZE, MAX_PAGE_SIZE, MIN_PAGE_SIZE } from "../../../../constants";
@@ -83,4 +83,42 @@ export const agentsRouter = createTRPCRouter({
 
       return data[0];
     }),
+    remove: protectedProcedure
+    .input(z.object({ id: z.string() }))
+    .mutation(async ({ input, ctx }) => {
+      const [removedAgent] = await db.delete(agents)
+      .where(
+        and(
+          eq(agents.id, input.id),
+          eq(agents.userId, ctx.auth.user.id)
+        ),
+      ).returning();
+
+      if(!removedAgent) {
+        throw new TRPCError({code: "NOT_FOUND", message: "Agent not found"});
+      }
+
+      return removedAgent;  
+    }),
+
+    updateAgent: protectedProcedure
+    .input(agentsUpdateSchema)
+    .mutation(async ({ input, ctx }) => {
+      const [updateAgent] = await db
+      .update(agents)
+      .set(input)
+      .where(
+        and(
+          eq(agents.id, input.id),
+          eq(agents.userId, ctx.auth.user.id)
+        )
+      ).returning();
+
+      if(!updateAgent) {
+         throw new TRPCError({code: "NOT_FOUND", message: "Agent not found"});
+      }
+
+      return updateAgent;
+    })
+
 });
