ae819af7bfc87cdfad66ff096a9ce9caaab3d1c4
Estifanos58 <estifkebe08@gmail.com>
2025-06-30 16:33:05 +0300

    feat: meating page ui and schema in progress

---
+++ src/app/(dashboard)/meetings/page.tsx
@@ -0,0 +1,23 @@
+import MeetingsView, { MeetingErrorView, MeetingLoadingView } from '@/modules/meetings/ui/views/meetings-view'
+import { getQueryClient, trpc } from '@/trpc/server'
+import { dehydrate, HydrationBoundary } from '@tanstack/react-query';
+import React, { Suspense } from 'react'
+import { ErrorBoundary } from 'react-error-boundary';
+
+function page() {
+  const queryClient = getQueryClient();
+  void queryClient.prefetchQuery(
+    trpc.meetings.getMany.queryOptions({}),
+  )
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <Suspense fallback={<MeetingLoadingView/>}>
+        <ErrorBoundary fallback={<MeetingErrorView/>}>
+           <MeetingsView />
+        </ErrorBoundary>
+      </Suspense>
+    </HydrationBoundary>
+  )
+}
+
+export default page
